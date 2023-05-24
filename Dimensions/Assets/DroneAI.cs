1b48fd9320d1f184730ba91abb7d6c5d9ef0bf82
David <djhatvr@gmail.com>
2023-05-23 22:44:11 -0700

    Include my Extension package, make simple Drone AI chase script

--- Dimensions/Assets/DroneAI.cs
+++ Dimensions/Assets/DroneAI.cs
@@ -8,15 +8,35 @@ public class DroneAI : MonoBehaviour
 {
     internal List<GameObject> players;
 
+    private Rigidbody rb;
+
     
     void Start()
     {
+        rb      = GetComponent<Rigidbody>();
         players = FindObjectsOfType<SUPERCharacterAIO>().ToList().ConvertAll(x => x.gameObject);
     }
 
 
+    private string lastPlayersName;
+    
     void Update()
     {
-        
+        var orderedPlayers = players.OrderBy(a => Vector3.Distance(a.gameObject.transform.position, transform.position)).ToList();
+        var closestPlayer  = orderedPlayers[0];
+        var playerDelta    = closestPlayer.transform.position - transform.position;
+        var dir            = playerDelta.normalized;
+
+        rb.velocity = dir;
+
+        var angRot = Vector3.Cross(dir, transform.forward);
+        rb.angularVelocity = angRot;
+
+        var playersName = closestPlayer.gameObject.name;
+        if (playersName != lastPlayersName)
+        {
+            Debug.Log($"Closest Player: {playersName}");
+            lastPlayersName = playersName;
+        }
     }
 }
