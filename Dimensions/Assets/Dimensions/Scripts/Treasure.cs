6dc76f2f3f34f51415543e6d417dab050ecf6726
AlMartson <6304456+almartson@users.noreply.github.com>
2023-06-20 22:22:07 -0500

    * Started with the process of: refactoring my own Classes in the Project (GameManager.cs, Treasure.cs, etc) to replace all "public" fields by "[SerializeField] private". I will also add some line comments where I see it's needed to make everything clearer. ( Worked on: Treasure.cs )
    * Corrected a misspell in Main Menu.

--- Dimensions/Assets/Dimensions/Scripts/Treasure.cs
+++ Dimensions/Assets/Dimensions/Scripts/Treasure.cs
@@ -1,42 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Serialization;
 
 public class Treasure : MonoBehaviour
 {
 
-	public int value = 1000;
-	public GameObject explosionPrefab;
+	[Tooltip("Treasure value.")]
+	[FormerlySerializedAs("value")]
+	[SerializeField]
+	private int _value = 1000;
+	
+	[Tooltip("Actor who will collide with this GameObject (i.e.: 'Player').")]
+	[SerializeField]
+	private string _collisionActorTag = "Player";
+
+	
+	[Tooltip("Prefab VFX (or 'Particle System') to be spawned in-place, when the Collision with this Treasure happens.")]
+	[FormerlySerializedAs("explosionPrefab")]
+	[SerializeField]
+	private GameObject _explosionPrefab;
 
 	[Tooltip("Destroy this GameObject with the collision?")]
 	[SerializeField]
-	public bool _destroyThisGameObject = false;
+	private bool _destroyThisGameObject = false;
 
 
+
+	#region Unity Methods
+
+	
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.gameObject.CompareTag("Player"))
+		if (other.gameObject.CompareTag( _collisionActorTag ))
 		{
 			if (GameManager.Gm != null)
 			{
 				// tell the game manager to Collect
-				GameManager.Gm.Collect (value);
+				GameManager.Gm.Collect (_value);
 				
 				// Make the Player Disappear, because he won (we don't want him to collect this Treasure multiple times..):
 				//
 				other.gameObject.SetActive(false);
 			}
 			
-			// explode if specified
-			if (explosionPrefab != null)
+			// Explode if specified
+			//
+			if (_explosionPrefab != null)
 			{
-				Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+				Instantiate (_explosionPrefab, transform.position, Quaternion.identity);
 			}
 			
-			// destroy after collection
+			// Destroy after collection
+			//
 			if (_destroyThisGameObject)
 			{
 				Destroy (gameObject);
 			}
 		}
-	}
+	}//End OnTriggerEnter
+
+	#endregion Unity Methods
+	
 }
